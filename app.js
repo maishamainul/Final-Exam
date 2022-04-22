75ecf1d703aefed4ae624520d2b741cedd9bb207
BC C <maishamainul.12@gmail.com>
2022-04-22 11:24:53 +0600

    third commit

--- app.js
+++ app.js
@@ -28,4 +28,17 @@ container.innerHTML = '<p><b> ${title} </b></p><p> Name: ${name} </p><p> MealID:
 container.appendchild (NewDiv);
 }
 
+
+function add() {
+    document.getElementById('saved').innerText = JSON.parse(localstorage.getItem('Meals'));
+    var typed = document.getElementById ('task').Value;
+    document.getElementById('Meals').value="";
+
+    var ui = document.getElementById('display');
+    var NewLi = document.createElement('li');
+    NewLi.innerText = typed;
+    localStorage.setItem ('meals', JSON.stringify(typed));
+    ui.appendchild(NewLi);
+}
+
                                                 
