f29d6740d932aa49149bbad7e38f17984cadb3e1
BC C <maishamainul.12@gmail.com>
2022-04-22 11:01:11 +0600

    second commit

---
+++ app.js
@@ -0,0 +1,31 @@
+var text = '';
+function searchmeal () {
+    text = document.getElementById (find-meal).value ;
+    connectMeal (text)
+}
+
+
+function connectMeal (text) {
+    fetch ('www.themealdb.com/api/json/v1/1/random.php')
+    .then (res=> res.json)
+    .then (data=> loadMeal (data));
+}
+
+connectMeal ();
+
+function loadMeal (data) {
+    console.table (data.Meals[0]);
+    var container=document.getElementById('main-container');
+
+    var title = data.Meals [0] .strMeal;
+    var cat = data.Meals [0] .strCategory;
+
+
+    //var newDiv = document.createElement("div");
+
+
+container.innerHTML = '<p><b> ${title} </b></p><p> Name: ${name} </p><p> MealID: ${ID} </p><img src="${/images/media/meals/llcbn01574260722.jpg/preview}" </br><p> Cooking Instruction: ${Instruction} </p>';
+container.appendchild (NewDiv);
+}
+
+                                                
